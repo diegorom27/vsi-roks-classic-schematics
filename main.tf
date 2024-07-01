c3a38e21f958cc493614801cc95c9cb564d3293b
diegorom27 <diegorom27@gmail.com>
2024-07-01 16:48:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "ibm_storage_block" "control_plane_storage" {
   }
   type = "Endurance"
   os_format_type = "Linux"
-  name           = "${each.key}"
+  id           = "${each.key}"
   datacenter       = "dal13"
   capacity       = each.value
   storage_type   = "performance" # or "standard"
@@ -65,7 +65,7 @@ resource "ibm_compute_vm_instance_block_device_attachment" "control_plane_storag
   }
   instance_id     = ibm_compute_vm_instance.control_plane[each.key].id
   block_volume_id = ibm_storage_block.control_plane_storage[each.key].id
-  device_name     = "xvd${char(97 + idx)}" # a, b, c, etc.
+  device_name     = "xvd${char(97 + tonumber(split("-", each.key)[1]))}" # a, b, c, etc.
 }
 
 ##############################################################################
@@ -78,7 +78,7 @@ resource "ibm_storage_block" "worker_nodes_storage" {
   }
   type = "Endurance"
   os_format_type = "Linux"
-  name         = "${each.key}"
+  id         = "${each.key}"
   datacenter     = "dal13"
   capacity     = 100 # Example capacity in GiB; adjust as needed
   share_protocol = "NFS" # Common protocol for file shares
@@ -104,8 +104,8 @@ resource "ibm_compute_vm_instance_block_device_attachment" "worker_nodes_storage
   for_each = {
     for vm in var.worker_nodes : vm.hostname => { for idx, size in vm.disks : "${vm.hostname}-${idx}" => size }
   }
-  instance_id     = ibm_compute_vm_instance.worker_nodes[split("-", each.key)[0]].id
-  block_volume_id = ibm_is_volume.worker_nodes_storage[each.key].id
+  instance_id     = ibm_compute_vm_instance.worker_nodes[each.key].id
+  block_volume_id = ibm_storage_block.worker_nodes_storage[each.key].id
   device_name     = "xvd${char(97 + tonumber(split("-", each.key)[1]))}" # a, b, c, etc.
 }
 ##############################################################################
@@ -118,7 +118,7 @@ resource "ibm_storage_block" "ODF_nodes_storage" {
   }
   type = "Endurance"
   os_format_type = "Linux"
-  name         = "${each.key}"
+  id         = "${each.key}"
   datacenter     = "dal13"
   capacity     = 100 # Example capacity in GiB; adjust as needed
   share_protocol = "NFS" # Common protocol for file shares
@@ -143,7 +143,7 @@ resource "ibm_compute_vm_instance_block_device_attachment" "ODF_storage_attachme
   for_each = {
     for vm in var.ODF : vm.hostname => { for idx, size in vm.disks : "${vm.hostname}-${idx}" => size }
   }
-  instance_id     = ibm_compute_vm_instance.ODF[split("-", each.key)[0]].id
-  block_volume_id = ibm_is_volume.ODF_nodes_storage[each.key].id
+  instance_id     = ibm_compute_vm_instance.ODF[each.key].id
+  block_volume_id = ibm_storage_block.ODF_nodes_storage[each.key].id
   device_name     = "xvd${char(97 + tonumber(split("-", each.key)[1]))}" # a, b, c, etc.
 }
